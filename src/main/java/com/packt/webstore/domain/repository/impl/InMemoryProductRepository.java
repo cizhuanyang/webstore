c4025921317c79a05423bc25752310b8449f2147
FrouFrou <yangyang55@outlook.com>
2017-10-10 21:20:15 +0800

    add exception handler

--- src/main/java/com/packt/webstore/domain/repository/impl/InMemoryProductRepository.java
+++ src/main/java/com/packt/webstore/domain/repository/impl/InMemoryProductRepository.java
@@ -7,12 +7,14 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.packt.webstore.domain.Product;
 import com.packt.webstore.domain.repository.ProductRepository;
+import com.packt.webstore.exception.ProductNotFoundException;
 
 @Repository
 public class InMemoryProductRepository implements ProductRepository{
@@ -77,7 +79,11 @@ public class InMemoryProductRepository implements ProductRepository{
       Map<String, Object> params = new HashMap<String, Object>();
       params.put("id", productID);
 
-      return jdbcTemplate.queryForObject(SQL, params, new ProductMapper());
+      try {
+          return jdbcTemplate.queryForObject(SQL, params, new ProductMapper());
+       } catch (DataAccessException e) {
+          throw new ProductNotFoundException(productID);
+       } 
    }
    
    @Override
